ebad2ecf0ae40e5e16381554e9b2c4ec9cd53331
edwaltz89 <edwaltz89@gmail.com>
2018-01-09 19:26:42 +0900

    로그인 시 출석 상황 초기 작업 추가.

--- src/main/java/xyz/btpink/w/AccountController.java
+++ src/main/java/xyz/btpink/w/AccountController.java
@@ -17,6 +17,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import xyz.btpink.w.dao.*;
 import xyz.btpink.w.vo.*;
+import xyz.btpink.w.vo.ClassVO;
+import xyz.btpink.w.dao.AttendenceDAO;
+import xyz.btpink.w.dao.ClassDAO;
 
 /**
  * Handles requests for the application home page.
@@ -25,6 +28,12 @@ import xyz.btpink.w.vo.*;
 public class AccountController {
 	@Autowired
 	AccountDAO accountDAO;
+	
+	@Autowired
+	ClassDAO cdao;
+	
+	@Autowired
+	AttendenceDAO adao;
 	
 	//로그인 검증하고 계정을 판별하여 반환함.
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
@@ -36,6 +45,20 @@ public class AccountController {
 		String result = accountDAO.Login(data);
 		if(result.substring(0,1).equals("T")){
 			//그날의 출석정보가 없으면 기본값을 입력하도록하는 SQL 
+			
+			System.out.println("초기확인작업 시작");
+
+			String memno = data.getId(); // 멤버 넘버 가져옴
+			ClassVO selClass = cdao.selectClass(memno); // 멤버 넘버에 할당된 클래스 VO 가져옴
+			String classno = selClass.getClassNo(); // 클래스 VO에 포함된 클래스 넘버 가져옴.
+
+			
+				System.out.println("클래스 넘버 : " + classno);
+
+				adao.initAtd(classno); // 출석부 표시전 초기 확인작업
+
+				System.out.println("초기작업 종료");
+			
 			
 		}
 		return result;
